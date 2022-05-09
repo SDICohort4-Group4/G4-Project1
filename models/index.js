78b081c92ad01a6e1a63578e7594641e34169a18
Wee Tien Guan <98148142+tgweeSDICohort4@users.noreply.github.com>
2022-05-09 13:05:03 +0800

    add heroku db credentials

--- models/index.js
+++ models/index.js
@@ -2,10 +2,20 @@
 const {Sequelize}=require("sequelize");
 
 // DB Configuration
-const sequelize = new Sequelize("d54c910kngshno2", "vkqhmwbduhvfmp", "4d87c51dab0ea7fc088128f077bb4cb6588faeb94500fb0c9a80a2bbeaa5aea5", {
-  host: "ec2-52-4-104-184.compute-1.amazonaws.com",
-  dialect: "postgres",
+// const sequelize = new Sequelize("d54c910kngshno2", "vkqhmwbduhvfmp", "4d87c51dab0ea7fc088128f077bb4cb6588faeb94500fb0c9a80a2bbeaa5aea5", {
+//   host: "ec2-52-4-104-184.compute-1.amazonaws.com",
+//   dialect: "postgres",
+// });
+
+const sequelize= new Sequelize(process.env.DATABASE_URL, {
+  dialectOptions: {
+    ssl: {
+      require:true,
+      rejectUnauthorized: false
+    }
+  }
 });
+
 
 // Import db models
 const AdminUser=require("./admin.user.model")(sequelize);
