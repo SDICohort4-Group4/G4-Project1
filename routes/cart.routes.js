de067484759e9683f4cb53401f09369b577b9ce6
Wee Tien Guan <98148142+tgweeSDICohort4@users.noreply.github.com>
2022-06-30 21:47:12 +0800

    add cart delete and save functionality

--- routes/cart.routes.js
+++ routes/cart.routes.js
@@ -4,6 +4,10 @@ const router = express.Router();
 const CartController = require("../controllers/cart.controller");
 const cartController = new CartController;
 
-router.get("/cart/:userid",cartController.getCartContents)
+router.get("/cart/:userid",cartController.getCartByUserID)
+
+router.post("/cart/save",cartController.saveCart)
+
+router.put("/cart/delete/:userid?/:itemid?",cartController.deleteCart)
 
 module.exports = router;
--- services/cart.service.js
+++ services/cart.service.js
@@ -1,6 +1,5 @@
-const {Cart, User, Item, sequelize} = require("../models");
+const {Cart, User, Item} = require("../models");
 const { Op } = require('sequelize')
-
 
 class CartService{
 
@@ -15,7 +14,7 @@ class CartService{
         "cartContentsID",
     ];
 
-    async getCartContents(userID){
+    async getCartByUserID(userID){
         let result = {
             message: null,
             status: null,
@@ -25,18 +24,18 @@ class CartService{
         let getCart=null;
 
         getCart=await Cart.findAll({
-            raw: true, //avoid nesting in result
+            raw: true, //avoid nesting in result object
             attributes: {exclude: this.excludeData},
             include: {
                 model: Item,
                 required: true,
                 attributes: {exclude: this.excludeData}
             },
-            where: {userID:`${userID}`},
+            where: {userID: userID},
         })
 
         if (getCart.length == 0){
-            result.message = `No cart found for userID:${userID}`;
+            result.message = `No cart found for userID: ${userID}`;
             result.status=404;
             return result;
         }
@@ -44,7 +43,45 @@ class CartService{
         result.message = "Cart Contents retrieved";
         result.data=getCart;
         result.status=200;
+        return result;
+    }
 
+    async saveCart(userID, itemID, itemQtyCart){
+        let result = {
+            message: null,
+            status: null,
+            data: null,
+        };
+
+        await Cart.create({
+            userID: userID,
+            itemID: itemID,
+            itemQtyCart: itemQtyCart,
+        })
+
+        result.message = "Cart Contents saved";
+        result.status=200;
+        return result;
+    }
+
+    async deleteCart(userID, itemID){
+        let result = {
+            message: null,
+            status: null,
+            data: null,
+        };
+
+        // if itemid is not present, delete all based on userid
+        if (!itemID){
+            await Cart.destroy({where: {userID: userID}})
+            result.message = `All Cart contents for userID:${userID} deleted`;
+        } //else delete based on userid and itemid
+        else {
+            await Cart.destroy({where: {userID: userID, itemID: itemID}})
+            result.message = `Only itemID:${itemID} for userID:${userID} deleted`;
+        }
+        
+        result.status=200;
         return result;
     }
 
